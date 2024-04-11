574caf3e0328bdde0540e8f82e56f3e89c17ebd3
Mottais <mottais.afy@outlook.fr>
2024-04-11 12:53:17 +0200

    Retour à mes fichiers

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""flask app to return web pages depending on routes"""
+"""Module for the different pages with Flask"""
 from flask import Flask, render_template
-from models.state import State
 from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """List all states"""
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=sorted_states)
+
+
 @app.teardown_appcontext
-def teardown_storage(something):
-    """this function is called at the end to close storage"""
+def close(exception):
+    """Close storage"""
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """print all states in storage"""
-    state_list = storage.all(State).values()
-    return render_template("7-states_list.html", all_states=state_list)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
